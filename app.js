1a86e85ffb4ac3736d7be63253a5f7a352a3b6ef
lupish <lubentofra@gmail.com>
2022-12-23 17:46:55 -0300

    Add img in browser tab

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get('/',(req,res)=>{
     let htmlPath=path.resolve(__dirname,'./views/home.html')
     res.sendFile(htmlPath)
 })
+app.get('/login',(req,res)=>{
+    let htmlPath=path.resolve(__dirname,'./views/login.html')
+    res.sendFile(htmlPath)
+})
 app.get('/register',(req,res)=>{
     let htmlPath=path.resolve(__dirname,'./views/register.html')
     res.sendFile(htmlPath)
