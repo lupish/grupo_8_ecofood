8a9b4fc3ddfa1f2c975dd45cf7e0f5b0bef0378f
sheine1234 <sheine.siro@gmail.com>
2023-01-16 13:51:00 -0300

    subo editProducts terminado

--- routes/products.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router;
-
-router.get("/newProduct", productController.newProduct)
-router.post("/newProduct", productController.create)
-
-router.get("/editProduct/:idProduct", productController.edit)
-module.exports = router;
--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -6,7 +6,7 @@ const productController = require('../controllers/productController')
 router.get('/productDetail/:idProd', productController.productDetail);
 router.get('/productCart', productController.productCart);
 router.get('/newProduct', productController.newProduct);
-router.get('/editProduct', productController.editProduct);
+router.get('/editProduct/:idProd?', productController.editProduct);
 router.get('/listProducts', productController.listProducts);
 
 module.exports = router;
