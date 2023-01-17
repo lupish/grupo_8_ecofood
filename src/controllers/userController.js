2cd9c5e9fa22c1e5bb7aeb8ff766b260c3adf9cd
sheine1234 <sheine.siro@gmail.com>
2023-01-17 15:02:56 -0300

    trabajando con crud para usuarios

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,32 @@
+const fs = require('fs');
 const controller = {
     login: (req, res) => {
         res.render('users/login')
     },
     register: (req, res) => {
         res.render('users/register')
+    },
+    createAcount: (req, res) => {
+       let usuario = {
+           nombre: req.body.nombre,
+           email: req.body.email,
+           contrasenia: req.body.contrasenia,
+           confirmarContrasenia: req.body.confirmarContrasenia
+       }
+    //GUARDAR
+    let archivoUsuario = fs.readFileSync('usersDB.json', {encoding: 'utf-8'});
+    let usuarios;
+    if(archivoUsuario==""){
+        usuarios = [];
+    }else{
+        usuarios = JSON.parse(archivoUsuario);
     }
+    usuarios.push(usuario);
+    usuariosJSON = JSON.stringify(usuarios);
+    fs.writeFileSync('usersDB.json', usuariosJSON);
+   
+    res.redirect('/home');
+ }
 }
 
 
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/userController');
-
+//LOGIN
 router.get('/login', userController.login);
+router.post('/login', userController.login);
+//REGISTER
 router.get('/register', userController.register);
-
+router.post('/register', userController.createAcount);
 
 module.exports = router;
