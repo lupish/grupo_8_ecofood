ca59b7a6a3220eaf01ccfc20c1eae43f07f444ef
lupish <lubentofra@gmail.com>
2023-03-23 20:34:03 -0300

    ver usuario

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -98,13 +98,16 @@ const controller = {
             }
             let rolesUsuario = await Rol.findAll({where: {nombre: "Usuario"}})
             let userRol = rolesUsuario[0].id
-            let user = await Usuario.create({
-                nombre: req.body.nombre,
-                email: req.body.email,
-                contrasenia: bcryptjs.hashSync(req.body.contrasenia, 10),
-                img: userImg,
-                rol_id: userRol
-            },{transaction: t})
+            let user = await Usuario.create(
+                {
+                    nombre: req.body.nombre,
+                    email: req.body.email,
+                    contrasenia: bcryptjs.hashSync(req.body.contrasenia, 10),
+                    img: userImg,
+                    rol_id: userRol
+                },
+                {transaction: t}
+            )
             await t.commit();
             res.redirect('/');
         } else {
@@ -122,136 +125,141 @@ const controller = {
     }
     },
     userDetail: async (req, res) => {
-    const t = await sequelize.transaction();
-    try{
-        //const user = await Usuario.findByPk({association: "rol"}, req.params.id);
-        const user = await Usuario.findOne({where: {id: req.params.id}}, {include:[{association: "rol"}], paranoid: false}, {transaction: t});
-        if (user) {
-            await t.commit();
-            res.render('users/userDetail', {user: user})
-        } else {
-            return res.redirect('/products/product-not-found');
-        }
-    }
-    catch (error){
-        await t.rollback();
-        console.log(error);
-    }
-    },
-    edit: async (req, res)=>{
-    const t = await sequelize.transaction();
-    try{
-        const user = await Usuario.findByPk(req.params.id, {paranoid: false},{transaction: t});
-        let rolUser;
-        if (req.session.usuarioLogueado) {
-            rolUser = await Rol.findByPk(req.session.usuarioLogueado.rol_id);
-            if (rolUser.nombre != "Administrador" && req.session.usuarioLogueado.id != user.id) {
+        const t = await sequelize.transaction();
+        try{
+            const user = await Usuario.findOne({
+                where: {id: req.params.id}
+                ,include: [{association: "rol"}],
+                paranoid: false,
+                transaction: t
+            });
+            
+            if (user) {
+                await t.commit();
+                res.render('users/userDetail', {user: user})
+            } else {
                 return res.redirect('/products/product-not-found');
             }
-        } else {
-            return res.redirect('/products/product-not-found');
-        }  
-        const roles = await Rol.findAll();
-        if (user) {
-            await t.commit();
-            res.render('users/edit', {user: user, rolUser: rolUser.nombre, rol: roles})
-        } else {
-            return res.redirect('/products/product-not-found');
         }
-    }
-    catch (error){
-        await t.rollback();
-        console.log(error);
-    }
+        catch (error){
+            await t.rollback();
+            console.log(error);
+        }
+    },
+    edit: async (req, res)=>{
+        const t = await sequelize.transaction();
+        try{
+            const user = await Usuario.findByPk(req.params.id, {paranoid: false},{transaction: t});
+            let rolUser;
+            if (req.session.usuarioLogueado) {
+                rolUser = await Rol.findByPk(req.session.usuarioLogueado.rol_id);
+                if (rolUser.nombre != "Administrador" && req.session.usuarioLogueado.id != user.id) {
+                    return res.redirect('/products/product-not-found');
+                }
+            } else {
+                return res.redirect('/products/product-not-found');
+            }  
+            const roles = await Rol.findAll();
+            if (user) {
+                await t.commit();
+                res.render('users/edit', {user: user, rolUser: rolUser.nombre, rol: roles})
+            } else {
+                return res.redirect('/products/product-not-found');
+            }
+        }
+        catch (error){
+            await t.rollback();
+            console.log(error);
+        }
     }, 
     processEdit: async (req, res) => {
-    const t = await sequelize.transaction();
-    try{
-        let userId = req.params.id;
-        let oldUser = await Usuario.findByPk(userId, {paranoid: false});
+        const t = await sequelize.transaction();
+        try{
+            let userId = req.params.id;
+            let oldUser = await Usuario.findByPk(userId, {paranoid: false});
 
-        if (req.session.usuarioLogueado) {
-            rolUser = await Rol.findByPk(req.session.usuarioLogueado.rol_id);
-        } else {
-            return res.redirect('/products/product-not-found');
-        }
-        const roles = await Rol.findAll();
+            if (req.session.usuarioLogueado) {
+                rolUser = await Rol.findByPk(req.session.usuarioLogueado.rol_id);
+            } else {
+                return res.redirect('/products/product-not-found');
+            }
+            const roles = await Rol.findAll();
 
-        const valRes = validationResult(req);
-        if (req.session.usuarioLogueado.id == userId && valRes.errors.length > 0) {
-            let userData = oldUser
-            userData.nombre =req.body.nombre
+            const valRes = validationResult(req);
+            if (req.session.usuarioLogueado.id == userId && valRes.errors.length > 0) {
+                let userData = oldUser
+                userData.nombre =req.body.nombre
 
-            return res.render('users/edit', { errors: valRes.mapped(), user: userData, rolUser: rolUser.nombre, rol: roles })
+                return res.render('users/edit', { errors: valRes.mapped(), user: userData, rolUser: rolUser.nombre, rol: roles })
+            }
+            
+            let userImg; 
+            if (req.file != undefined) {
+                userImg = "/img/users/" + req.file.filename;
+            } else {
+                userImg = oldUser.img
+            }
+            let user = await Usuario.update({
+                nombre: req.body.nombre,
+                img: userImg,
+                rol_id: req.body.rol
+            },
+            {
+                where: {id: userId},
+                paranoid: false
+            },{transaction: t})
+            if (req.session.usuarioLogueado.id == userId) {
+                let userEditado = await Usuario.findByPk(userId)
+                req.session.usuarioLogueado = userEditado;
+            }
+            await t.commit();
+            return  res.redirect('/users/userDetail/' + userId)
         }
-        
-        let userImg; 
-        if (req.file != undefined) {
-            userImg = "/img/users/" + req.file.filename;
-        } else {
-            userImg = oldUser.img
+        catch (error){
+            await t.rollback();
+            console.log(error);
         }
-        let user = await Usuario.update({
-            nombre: req.body.nombre,
-            img: userImg,
-            rol_id: req.body.rol
-        },
-        {
-            where: {id: userId},
-            paranoid: false
-        },{transaction: t})
-        if (req.session.usuarioLogueado.id == userId) {
-            let userEditado = await Usuario.findByPk(userId)
-            req.session.usuarioLogueado = userEditado;
-        }
-        await t.commit();
-        return  res.redirect('/users/userDetail/' + userId)
-    }
-    catch (error){
-        await t.rollback();
-        console.log(error);
-    }
     },
     softDelete: async (req, res)=>{
-    const t = await sequelize.transaction();
-    try{
-        let userId = req.params.id;
-        let user = await Usuario.destroy({where: {id: userId}},{transaction: t})
-        await t.commit();
-        return res.redirect('/panels/manageUsers/');
-    }
-    catch (error){
-        await t.rollback();
-        console.log(error);
-    }
+        const t = await sequelize.transaction();
+        try{
+            let userId = req.params.id;
+            let user = await Usuario.destroy({where: {id: userId}},{transaction: t})
+            await t.commit();
+            return res.redirect('/panels/manageUsers/');
+        }
+        catch (error){
+            await t.rollback();
+            console.log(error);
+        }
     },
     processActivate: async (req, res) => {
-    const t = await sequelize.transaction();
-    try{
-        let userId = req.params.id;
-        const user = await Usuario.restore({
-            where: {id : userId}
-        },{transaction: t})
-        await t.commit();
-        return res.redirect('/panels/manageUsers/');
-    }
-    catch (error){
-        await t.rollback();
-        console.log(error);
-    }
+        const t = await sequelize.transaction();
+        try{
+            let userId = req.params.id;
+            const user = await Usuario.restore({
+                where: {id : userId}
+            },{transaction: t})
+            await t.commit();
+            return res.redirect('/panels/manageUsers/');
+        }
+        catch (error){
+            await t.rollback();
+            console.log(error);
+        }
     },
     logout: async (req, res) => {
-    const t = await sequelize.transaction();
-    try{
-        res.clearCookie('email')
-        req.session.destroy();
-        await t.commit();
-        return res.redirect('/')
-    }
-    catch (error){
-        await t.rollback();
-        console.log(error);
-    }
+        const t = await sequelize.transaction();
+        try{
+            res.clearCookie('email')
+            req.session.destroy();
+            await t.commit();
+            return res.redirect('/')
+        }
+        catch (error){
+            await t.rollback();
+            console.log(error);
+        }
     }
 }
 module.exports = controller;
