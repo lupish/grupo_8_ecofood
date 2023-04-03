57e52cc5206e13df1d5b890fc991a42d775d26d7
lupish <lubentofra@gmail.com>
2023-04-02 21:26:03 -0300

    Validaciones usuario back: largo del nombre y forma de la pass

--- src/modulos/validaciones/validacionesUsuario.js
+++ src/modulos/validaciones/validacionesUsuario.js
@@ -2,7 +2,7 @@ const { body } = require('express-validator');
 function validar(tipoValidacion) {
     // validaciones genericas de usuarui
     let validaciones = [
-        body("nombre").notEmpty().withMessage("Debe ingresar un nombre"),
+        body("nombre").isLength({min: 2}).withMessage("El nombre debe tener al menos 2 caracteres"),
         body("user_foto").custom((value, { req }) => {
             const extensions = ['.jpg', '.png', '.gif', '.jpeg']
             const fileError = req.fileError;
@@ -17,7 +17,14 @@ function validar(tipoValidacion) {
 
     if (tipoValidacion == "Registro") {
         // validaciones para registro de usuario
-        validaciones.push(body("contrasenia").notEmpty().withMessage("Debe ingresar una contraseña"))
+        validaciones.push(body("contrasenia").isStrongPassword({
+            minLength: 8,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1,
+        }).withMessage("La contraseña debe tener al menos 8 caracteres, letras mayúsculas y minúsculas, un número y un símbolo"))
+        
         validaciones.push(body("email").isEmail().withMessage("Debe ingresar un mail válido"))
         validaciones.push(body("contrasenia").notEmpty().withMessage("Debe ingresar una contraseña"))
         validaciones.push(body("confirmarContrasenia").notEmpty().withMessage("Debe confirmar la contraseña"))
