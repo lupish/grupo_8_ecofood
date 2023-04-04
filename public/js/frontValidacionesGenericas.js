5d7360a00667411b2c4bde0e0ece60fa25ab4378
lupish <lubentofra@gmail.com>
2023-04-04 20:46:29 -0300

    validaciones genericas front: nombre y foto

---
+++ public/js/frontValidacionesGenericas.js
@@ -0,0 +1,47 @@
+window.onload = function () {
+    console.log("validaciones genericas en front")
+    form = document.getElementById("form-generico");
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault()
+
+        // obtener campos del formulario
+        let nombre = document.getElementById("obj-nombre");
+        let fotos = document.getElementById("obj-foto").files;
+
+        // limpiar mensajes de error
+        document.getElementById("p-obj-nombre").innerText = ""
+
+        // remover clases de errores para los inputs
+        nombre.classList.remove('input-form-no-aceptado');
+        document.getElementById("form-submit").classList.remove('boton-form-error');
+
+
+        errores = []
+        if (nombre.value.length == 0) {
+            errores.push("Debe ingresar un nombre")
+            nombre.classList.add('input-form-no-aceptado')
+            document.getElementById("p-obj-nombre").innerText = "Debe ingresar un nombre";
+        }
+        if (fotos != undefined && fotos.length > 0) {
+            let foto = fotos[0];
+            const extensions = ['.jpg', '.png', '.gif', '.jpeg'];
+            if (
+                !(foto.type.includes("jpg")  || foto.type.includes("jpeg")
+                || foto.type.includes("gif") || foto.type.includes("png"))
+            ) {
+                errores.push(`Las extensiones permitidas son : ${extensions.join(", ")}`)
+                document.getElementsByClassName("div-prod-fotos")[0].classList.add('input-form-no-aceptado')
+                document.getElementById("p-obj-foto").innerText = `Las extensiones permitidas son : ${extensions.join(", ")}`;
+            }            
+        }
+
+        if (errores.length == 0) {
+            form.submit()
+        } else {
+            document.getElementById("form-submit").classList.add('boton-form-error')
+        }
+
+    })
+
+}
