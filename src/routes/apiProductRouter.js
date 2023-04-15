1e03d6986d471a3f89376e2816df647b11bdccfc
lupish <lubentofra@gmail.com>
2023-04-14 22:09:26 -0300

    API productos: primeros pasos

---
+++ src/routes/apiProductRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = router;
+
+// CONTROLLER
+const productController = require('../controllers/apiProductController');
+
+// RUTAS
+router.get('/', productController.listAllProducts);
+router.get('/listProductsByLifeStyle/:estiloVidaId', productController.listProductsByLifeStyle);
