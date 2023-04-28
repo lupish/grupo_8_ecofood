521322642430f621aea3bdd2bd146cb781ae11d3
sheine1234 <65140413+sheine1234@users.noreply.github.com>
2023-04-27 21:35:05 -0300

    buscador

--- public/js/searchBar.js
+++ public/js/searchBar.js
@@ -17,52 +17,56 @@ async function fetchApi(endpoint){
 }
 
 async function readyDoc(){
+    
     const productos = await fetchApi('/api/products')
-    displayProds(productos.products)
     let searchBar = document.getElementById("buscador")
     searchBar.addEventListener('change', (e)=>{
-        filter(e.target.value, productos.products)
+        filter( e.target.value, productos.products)
     })
 }
 
 function displayProds(products){
-     let container = document.getElementById('contenedor-productos')
-     container.innerHTML = ``
+     let main = document.querySelector('main')
+     main.innerHTML = ``
      for (let i = 0; i < products.length; i++) {
-     container.innerHTML += `
+     main.innerHTML += `
+     
+     <section>
+     <div class="contenedor-productos" id="contenedor-productos" >
      <article class="article-prod">
      <a href="/products/productDetail/${products[i].id}">
          <div class="info-de-producto">
              <i id="estrella" class="fa-solid fa-star"></i>
                  <img src="${products[i].imagen}" alt="" width="360">
              <div><p>${products[i].nombre} - ${products[i].marca}</p></div> 
-             <div><p>$${products[i].precio}</p></div>
+             <div><p>${products[i].precio}</p></div>
          </div>
      </a>
- </article>
+    </article>
+    </div>
+    </section>
 
      `
      }
-     if( container.innerHTML === ''){
+     if( main.innerHTML === ''){
         window.location.replace(
             'http://localhost:3000/products/product-not-found'
           );
      }
 }
 
-function filter(busqueda, productos){
-
-    productos.sort((a,b)=>{
-        if(a.nombre < b.nombre) return -1
-        if(a.nombre > b.nombre) return 1
-        return 0
-    })
-
-    if(busqueda == ''){
-        displayProds(productos)
-    }else{
-        let filtro = productos.filter(elem => elem.nombre.toLowerCase().includes(busqueda.toLowerCase()) || elem.descripcionCorta.toLowerCase().includes(busqueda.toLowerCase()) ||  elem.descripcionLarga.toLowerCase().includes(busqueda.toLowerCase()) )
-        displayProds(filtro)
-    }
+function filter( busqueda, productos){
+        productos.sort((a,b)=>{
+            if(a.nombre < b.nombre) return -1
+            if(a.nombre > b.nombre) return 1
+            return 0
+        })
+    
+        if(busqueda == ''){
+            displayProds(productos)
+        }else{
+            let filtro = productos.filter(elem => elem.nombre.toLowerCase().includes(busqueda.toLowerCase()) || elem.descripcionCorta.toLowerCase().includes(busqueda.toLowerCase()) ||  elem.descripcionLarga.toLowerCase().includes(busqueda.toLowerCase()) )
+            displayProds(filtro)
+        }
 
 }
